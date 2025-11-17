a2b6eacd5fc8ad64cc772691485d9080c54c14ac
mohammadi <b.mohammadi.m@gmail.com>
2025-11-17 14:41:50 +0330

    modify add veriify code in verify code page

--- pages/login/index.js
+++ pages/login/index.js
@@ -50,7 +50,7 @@ const Index = () => {
         if (data?.data?.status) {
           if (data?.data?.data?.otp_sended) {
             router.push(
-              `/two_step_login/verify_code?username=${getValues("username")}`
+              `/two_step_login/verify_code?username=${encodeURIComponent(fullNumber)}`
             );
           } else {
             localStorage.setItem("token", data?.data?.data?.access_token);
